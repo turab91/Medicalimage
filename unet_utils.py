d44271bc151f9fcbd506bae9867b1878efa4eb13
Naimul Turab <turab91@gmail.com>
2018-05-07 20:49:53 +0200

    corrected files

--- unet_utils.py
+++ unet_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import h5py
 
-
+DATA_PATH = ".."
 def load_dataset():
     dataset = h5py.File(DATA_PATH, "r")
 
@@ -9,6 +9,5 @@ def load_dataset():
     train_set_y_orig = np.array(dataset["train_label"][:])
 
     test_set_x_orig = np.array(dataset["test_img"][:])
-    sizes_test = np.array(dataset["sizes_test"][:])
 
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, sizes_test
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig
